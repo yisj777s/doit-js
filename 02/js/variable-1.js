4bc73100fd9f211e120c678f25c3757814e95e4a
yisj777s <yisj777s@naver.com>
2024-02-20 19:13:09 +0900

    complete

--- 02/js/variable-1.js
+++ 02/js/variable-1.js
@@ -3,6 +3,6 @@ function add(a, b) {
 }
 
 var sum = add(10, 20);
-console.log(sum);
-var sum = 100;   // var 변수는 재선언, 재할당 가능
-console.log(sum);
+console.log(sum); // 30
+var sum = 100;    // var 변수는 재선언, 재할당 가능
+console.log(sum); // 100
