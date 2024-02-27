e079002a23143af44275c23ae2fb01e7f960404c
yisj777s <yisj777s@naver.com>
2024-02-27 10:19:39 +0900

    add example-04-마무리문제01(양수인지음수인지판단하기)

---
+++ 04/js/solution-1-example.js
@@ -0,0 +1,14 @@
+function isPositive(n) {
+  if (n > 0){
+    alert(`${n}은 양수입니다.`);
+  } else if (n < 0){
+    alert(`${n}은 음수입니다.`);
+  } else {
+    alert(`0입니다.`);
+  }
+}
+
+const userNumber = parseInt(prompt("숫자를 입력하세요.")); // parseInt()는 숫자가 아닐 경우 NaN을 반환함.
+if(!isNaN(userNumber)){ // 숫자일 경우에만 실행
+  isPositive(userNumber);
+}
