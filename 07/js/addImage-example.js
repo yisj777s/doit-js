dae58de46f46609edad62657eaff5e086c9a4bde
yisj777s <yisj777s@naver.com>
2024-03-06 18:23:06 +0900

    add practice-07-2-02-시간에 따라 다른 이미지 표시하기

---
+++ 07/js/addImage-example.js
@@ -0,0 +1,8 @@
+const today = new Date(); // 현재 날짜와 시간 정보를 담은 today 객체
+const hrs = today.getHours(); // 현재 시간 중 시 hour 정보를 가져옴.
+const container = document.querySelector("#container");
+
+let newImg = document.createElement("img");
+newImg.src = (hrs < 12) ? "images/morning.jpg" : "images/afternoon.jpg"
+container.appendChild(newImg);
+
