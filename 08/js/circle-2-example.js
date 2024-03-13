27c740201a62a1aa7fe310136e8004c408505a03
yisj777s <yisj777s@naver.com>
2024-03-13 18:09:08 +0900

    add example-08-3-05-반지름값을 입력받아 원의 넓이와 둘레 구하기(반올림 추가)

---
+++ 08/js/circle-2-example.js
@@ -0,0 +1,33 @@
+// 원의 넓이 구하기
+function area(r) {
+  return Math.PI * r * r;
+}
+
+// 원의 둘레 구하기
+function circum(r) {
+  return 2 * Math.PI * r;
+}
+
+const result = document.querySelector("#result"); 
+const radius = prompt("반지름의 크기는?");
+
+// 반올림 하지 않았을 때
+// result.innerText = `
+//     반지름 : ${radius},
+//     원의 넓이 : ${area(radius)},
+//     원의 둘레 : ${circum(radius)}
+//     `;
+
+// 소수점 이하 셋째 자리까지 표시.
+// result.innerText = `
+//     반지름 : ${radius},
+//     원의 넓이 : ${area(radius).toFixed(3)},
+//     원의 둘레 : ${circum(radius).toFixed(3)}
+//     `;
+
+// 결괏값 반올림 했을 때
+    result.innerText = `
+    반지름 : ${radius},
+    원의 넓이 : ${Math.round(area(radius))},
+    원의 둘레 : ${Math.round(circum(radius))}
+    `;
