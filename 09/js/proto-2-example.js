6ccb39bf2405ebd4ad63eba3ff878a2a8a67ee34
yisj777s <yisj777s@naver.com>
2024-03-22 12:21:14 +0900

    add example-09-4-08-prototype 프로퍼티로 생성자함수의 메서드 정의, 프로토타입 객체에 메서드 추가

---
+++ 09/js/proto-2-example.js
@@ -0,0 +1,16 @@
+function newBook (title, pages, done) {
+  this.title = title;
+  this.pages = pages;
+  this.done = done;
+}
+
+newBook.prototype.finish = function() {
+  this.done === false ? str = "읽는 중" : str = "완독";
+  return str;
+}
+
+const nBook1 = new newBook("웹 표준의 정석", 648, false);
+const nBook2 = new newBook("점프 투 파이썬", 360, true);
+
+console.log(nBook1.finish());
+console.log(nBook2.finish());
