35a66ec66e368db9422622568d5bb45c7867d53d
yisj777s <yisj777s@naver.com>
2024-04-24 12:33:54 +0900

    add example-12-마무리 문제01-서버에 JSON 자료 가져와 화면에 표시.

---
+++ 12/js/solution-1-example.js
@@ -0,0 +1,34 @@
+let jsonLocation = "https://reqres.in/api/products/10";
+const result = document.querySelector("#result");
+
+let xhr = new XMLHttpRequest();
+xhr.open("GET", jsonLocation);
+xhr.send();
+
+xhr.onreadystatechange = () => {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    let product = JSON.parse(xhr.responseText);
+    console.log(product);
+    result.innerHTML = `<p>상품명 : ${product.data.name}</p>
+    <p>생산년도 : ${product.data.year}</p>`;
+  }
+}
+
+// solution
+// const url = "https://reqres.in/api/products/10"
+// const result = document.querySelector("#result");
+
+// let xhr = new XMLHttpRequest();
+// xhr.open('GET', url, true);
+// xhr.send();
+
+// xhr.onreadystatechange = function () {
+//   if (xhr.readyState === 4 && xhr.status === 200) {
+//     let product = JSON.parse(xhr.responseText);
+//     console.log(product);
+//     result.innerHTML = `
+//       <p>상품명 : ${product.data.name}</p>
+//       <p>생산년도 : ${product.data.year}</p>
+//       `;
+//     }
+//   }
