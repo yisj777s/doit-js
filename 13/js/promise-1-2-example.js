87956ccc9f652edfc17ca2f892534f0487b59a28
yisj777s <yisj777s@naver.com>
2024-04-26 18:33:22 +0900

    add example-13-2-2-1.프로미스에서 성공 결과를 보냈을 때 실행하는 then

---
+++ 13/js/promise-1-2-example.js
@@ -0,0 +1,19 @@
+//13-2.프로미스
+//13-2-2.promise 객체 사용하기
+//프로미스에서 성공 결과를 보냈을 때 실행하는 then
+let likePizza = true;
+const pizza = new Promise((resolve, reject) => {
+  if (likePizza)
+    resolve('피자를 주문합니다.');
+  else
+    reject('피자를 주문하지 않습니다.');
+})
+// console.log(pizza);
+
+pizza
+  .then(
+    result => console.log(result)
+  )
+  .catch(
+    err => console.log(err)
+  );
